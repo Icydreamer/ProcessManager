cf721cf10f47bcf5389194612a64e234b78f5281
Icydreamer <1091939105@qq.com>
2023-05-21 22:50:30 +0800

    SQlite数据库创建

---
+++ DataBase/SQLite/MyDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using DataBase.Models;
+
+namespace DataBase.SQLite
+{
+    public class MyDbContext : DbContext
+    {
+        /// <summary>
+        /// 每日数据
+        /// </summary>
+        public DbSet<DailyLogModel> DailyLog { get; set; }
+        public DbSet<AppModel> App { get; set; }
+        /// <summary>
+        /// 分类
+        /// </summary>
+        public DbSet<CategoryModel> Categorys { get; set; }
+
+        private static string dbFilePath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "Data", "data.db");
+
+        
+    }
+}
