4f54d1b36d3c7da907533550ce87a5bf76d371c3
Icydreamer <1091939105@qq.com>
2023-05-24 23:02:09 +0800

    数据库实体操作

--- DataBase/Services/DataBase.cs
+++ DataBase/Services/DataBase.cs
@@ -6,10 +6,64 @@ using System.Threading.Tasks;
 using DataBase.SQLite;
 using DataBase.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Threading;
 namespace DataBase.Services
 {
     public class DataBase
     {
-        private readonly 
+        private MyDbContext _dbContext;
+        private readonly object _locker = new object();
+        private bool _locked = false;
+        private int _outTime = 10;
+        public DataBase(MyDbContext context)
+        {
+            _dbContext = context;
+        }
+        public MyDbContext GetReaderContext()
+        {
+            lock (_locker)
+            {
+                int lockTime = 0;
+                while (_locked)
+                {
+                    Thread.Sleep(100);
+                    lockTime += 100;
+                    if (lockTime >= _outTime * 1000)
+                    {
+                        _locked = false;
+                    }
+                }
+                //_locked = true;
+
+                //_readerContext = new TaiDbContext();
+                //return _readerContext;
+                return new MyDbContext();
+            }
+        }
+        public MyDbContext GetWriterContext()
+        {
+            lock (_locker)
+            {
+                int lockTime = 0;
+                while (_locked)
+                {
+                    Thread.Sleep(100);
+                    lockTime += 100;
+                    if (lockTime >= _outTime * 1000)
+                    {
+                        _locked = false;
+                    }
+                }
+                _locked = true;
+
+                _dbContext = new MyDbContext();
+                return _dbContext;
+            }
+        }
+        public void CloseWriter()
+        {
+            _dbContext?.Dispose();
+            _locked = false;
+        }
     }
 }
