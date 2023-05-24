80b15db7db7827c24a541cfa9dcf1058ac657ade
Icydreamer <1091939105@qq.com>
2023-05-24 22:52:39 +0800

    数据库实体操作

--- DataBase/Services/DataBase.cs
+++ DataBase/Services/DataBase.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DataBase.SQLite;
+using DataBase.Models;
+using Microsoft.EntityFrameworkCore;
 namespace DataBase.Services
 {
     public class DataBase
     {
+        private readonly 
     }
 }
