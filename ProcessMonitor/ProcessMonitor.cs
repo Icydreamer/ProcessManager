3b3df24f8d841dda8c68dc4041d805dbde9b8979
immengzi <13017606416@163.com>
2023-05-26 00:03:39 +0800

    添加程序入口点
    负责启动软件监控

--- ProcessMonitor/ProcessMonitor.cs
+++ ProcessMonitor/ProcessMonitor.cs
@@ -11,6 +11,16 @@ using System.Windows.Media.Media3D;
 
 namespace ProcessMonitor
 {
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var processMonitorMonitor = new ProcessMonitor();
+    //        processMonitorMonitor.StartMonitoring();
+
+    //        Console.ReadLine();
+    //    }
+    //}
     class ProcessMonitor
     {
         private string activeProcessName;
