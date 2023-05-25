68c1d7e2f8d7ca1470d132a687f805bd647de5cc
immengzi <13017606416@163.com>
2023-05-26 02:31:35 +0800

    添加配置项从文件 Config.json 读取
    未实现开机自启具体逻辑

--- ProcessMonitor/ProcessMonitor.cs
+++ ProcessMonitor/ProcessMonitor.cs
@@ -15,6 +15,21 @@ namespace ProcessMonitor
     //{
     //    static void Main(string[] args)
     //    {
+    //        var config = new Config();
+    //        config.LoadConfig();
+
+    //        bool startupEnabled = config.StartupEnabled;
+    //        string themeColor = config.ThemeColor;
+
+    //        if (startupEnabled)
+    //        {
+    //            // 执行开机自启逻辑
+    //            Console.WriteLine("开机自启已启用");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("开机自启已禁用");
+    //        }
     //        var processMonitorMonitor = new ProcessMonitor();
     //        processMonitorMonitor.StartMonitoring();
 
