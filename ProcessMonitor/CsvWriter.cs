561c681b36cba2ccb2b521ca801cc49a4377671a
immengzi <13017606416@163.com>
2023-05-25 23:58:06 +0800

    添加 CsvWriter 类
    负责将记录写入 CSV 文件

---
+++ ProcessMonitor/CsvWriter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Media.Media3D;
+
+namespace ProcessMonitor
+{
+    static class CsvWriter
+    {
+        public static void WriteRecordsToCsv(List<Record> records, string filePath)
+        {
+            using (var writer = new StreamWriter(filePath, false, Encoding.UTF8))
+            {
+                writer.WriteLine("ProcessName,Duration");
+
+                foreach (var record in records)
+                {
+                    writer.WriteLine($"{record.ProcessName},{record.FormattedDuration}");
+                }
+            }
+        }
+    }
+}
