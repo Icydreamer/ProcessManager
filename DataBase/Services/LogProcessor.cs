f0d3d082ac448eb295fee9d1b05593c462aa11bf
Icydreamer <1091939105@qq.com>
2023-05-27 20:23:31 +0800

    创建从日志写入数据库类

---
+++ DataBase/Services/LogProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataBase.Models;
+using DataBase.SQLite;
+namespace DataBase.Services
+{
+    /// <summary>
+    /// 从日志写入数据库类
+    /// </summary>
+    public class LogProcessor
+    {
+        private readonly MyDbContext DbContext;
+        public LogProcessor()
+        {
+            DbContext = new MyDbContext();
+        }
+
+    }
+}
