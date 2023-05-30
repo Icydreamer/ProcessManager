8f65555efdee24992c2d826ed89ca6294bd59f04
immengzi <13017606416@163.com>
2023-05-30 20:41:31 +0800

    添加系统托盘&&后台运行

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,15 @@
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using CommunityToolkit.Mvvm.Messaging.Messages;
+using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.ComponentModel;
+using MvvmTutorials.ToolkitMessages.Views;
+
 //using MvvmTutorials.ToolkitMessages.Models;
 
 namespace MvvmTutorials.ToolkitMessages.ViewModels;
@@ -61,4 +70,94 @@ public partial class MainWindowViewModel: ObservableRecipient
 
         PageSource = "HomePage.xaml";
     }
+
+    private TaskbarIcon taskbarIcon;
+    private bool isWindowVisible = true;
+
+    public MainWindowViewModel()
+    {
+        // 创建系统托盘图标
+        taskbarIcon = new TaskbarIcon();
+        taskbarIcon.Icon = new System.Drawing.Icon(".\\Resources\\1.ico"); // 设置图标路径
+        taskbarIcon.ToolTipText = "白驹";
+
+        // 添加双击图标事件
+        taskbarIcon.TrayMouseDoubleClick += TaskbarIcon_DoubleClick;
+
+        // 添加右键菜单
+        taskbarIcon.ContextMenu = new ContextMenu();
+        taskbarIcon.ContextMenu.Items.Add(new MenuItem { Header = "主页面", Name = "MainPageMenuItem" });
+        taskbarIcon.ContextMenu.Items.Add(new MenuItem { Header = "退出", Name = "ExitMenuItem" });
+
+        // 处理右键菜单项的点击事件
+        foreach (MenuItem menuItem in taskbarIcon.ContextMenu.Items)
+        {
+            menuItem.Click += MenuItem_Click;
+        }
+        // 处理窗口的 Closing 事件
+        Application.Current.MainWindow.Closing += MainWindow_Closing;
+    }
+
+    private void TaskbarIcon_DoubleClick(object sender, RoutedEventArgs e)
+    {
+        ToggleWindowVisibility();
+    }
+
+    private void MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        MenuItem clickedItem = e.Source as MenuItem;
+        if (clickedItem != null)
+        {
+            if (clickedItem.Name == "MainPageMenuItem")
+            {
+                ShowWindow();
+            }
+            else if (clickedItem.Name == "ExitMenuItem")
+            {
+                taskbarIcon.Dispose();
+                Application.Current.Shutdown();
+            }
+        }
+    }
+
+    private void ToggleWindowVisibility()
+    {
+        if (isWindowVisible)
+        {
+            HideWindow();
+        }
+        else
+        {
+            ShowWindow();
+        }
+    }
+
+    private void HideWindow()
+    {
+        Application.Current.MainWindow.Hide();
+        isWindowVisible = false;
+    }
+
+    private void ShowWindow()
+    {
+        Application.Current.MainWindow.Show();
+        Application.Current.MainWindow.WindowState = WindowState.Normal;
+        Application.Current.MainWindow.Activate();
+        isWindowVisible = true;
+    }
+
+    private void MainWindow_Closing(object sender, CancelEventArgs e)
+    {
+        // 取消窗口的关闭操作
+        e.Cancel = true;
+        // 隐藏窗口
+        HideWindow();
+    }
+
+    // 清理资源
+    public void Dispose()
+    {
+        taskbarIcon.Dispose();
+    }
+
 }
