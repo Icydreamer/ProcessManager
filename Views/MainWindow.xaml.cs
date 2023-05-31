b8c0babef6b418e1aa3c50453f0f8e6ce26f8b5c
wei_bu <663748426@qq.com>
2023-05-31 21:02:15 +0800

    按钮效果

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -4,9 +4,12 @@ using System.Security.Cryptography.X509Certificates;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Media;
 using HandyControl.Controls;
 using ScottPlot;
 using ScottPlot.Styles;
+using System.Globalization;
+
 
 namespace MvvmTutorials.ToolkitMessages.Views
 {
@@ -22,32 +25,63 @@ namespace MvvmTutorials.ToolkitMessages.Views
         private Set Set = new Set();
         public MainWindow()
             {
+            colors newcolor = new colors()
+            {
+                br1 = new System.Windows.Media.SolidColorBrush(Colors.AliceBlue),
+                br2 = new System.Windows.Media.SolidColorBrush(Colors.White),
+                br3 = new System.Windows.Media.SolidColorBrush(Colors.White),
+                br4 = new System.Windows.Media.SolidColorBrush(Colors.White),
+                br5 = new System.Windows.Media.SolidColorBrush(Colors.White),
+                YY=-590
+            };
             UserContent = HomePage;
             InitializeComponent();
+            ButtonGroup.DataContext = newcolor;
             this.DataContext = this;
+       
         }
 
         private void ButtonClick1(object sender, RoutedEventArgs e)
         {
             UserContent = HomePage;//内容呈现器绑定的UserContent赋值给用户控件1
+            colors newcolor = new colors();
+            newcolor.YY = -590;
+            newcolor.br1 = new System.Windows.Media.SolidColorBrush(Colors.AliceBlue);
+            ButtonGroup.DataContext = newcolor;
         }
 
         private void ButtonClick2(object sender, RoutedEventArgs e)
         {
             UserContent = SingleApp;//内容呈现器绑定的UserContent赋值给用户控件2
+            colors newcolor = new colors();
+            newcolor.YY = -510;
+            newcolor.br2 = new System.Windows.Media.SolidColorBrush(Colors.AliceBlue);
+            ButtonGroup.DataContext = newcolor;
         }
 
         private void ButtonClick3(object sender, RoutedEventArgs e)
         {
             UserContent = AllApps;//内容呈现器绑定的UserContent赋值给用户控件3
+            colors newcolor = new colors();
+            newcolor.YY = -430;
+            newcolor.br3 = new System.Windows.Media.SolidColorBrush(Colors.AliceBlue);
+            ButtonGroup.DataContext = newcolor;
         }
         private void ButtonClick4(object sender, RoutedEventArgs e)
         {
             UserContent = Monitor;//内容呈现器绑定的UserContent赋值给用户控件4
+            colors newcolor = new colors();
+            newcolor.YY = -350;
+            newcolor.br4 = new System.Windows.Media.SolidColorBrush(Colors.AliceBlue);
+            ButtonGroup.DataContext = newcolor;
         }
         private void ButtonClick5(object sender, RoutedEventArgs e)
         {
             UserContent = Set;//内容呈现器绑定的UserContent赋值给用户控件5
+            colors newcolor = new colors();
+            newcolor.YY = -50;
+            newcolor.br5 = new System.Windows.Media.SolidColorBrush(Colors.AliceBlue);
+            ButtonGroup.DataContext = newcolor;
         }
 
         private UserControl _content;
@@ -61,7 +95,8 @@ namespace MvvmTutorials.ToolkitMessages.Views
                 OnPropertyChanged("UserContent");
             }
         }
-
+  
+        //动态绑定的事件触发
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged(string propertyName)
         {
@@ -69,5 +104,19 @@ namespace MvvmTutorials.ToolkitMessages.Views
                 PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
         }
+
+        //按钮颜色
+       public class colors
+        {
+            public Brush br1 { get; set ; } 
+            public Brush br2 { get; set; }
+            public Brush br3 { get; set; }
+            public Brush br4 { get; set; }
+            public Brush br5 { get; set; }
+            public int YY { set; get; }
+        }
+
+
     }
+
 }
