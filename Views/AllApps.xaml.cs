8512281bd47d49bb2795ea5cf59e7304256e4def
HomesAmaranta <q2929736572@outlook.com>
2023-06-01 15:17:56 +0800

    所有应用按周、月获取使用时长

--- Views/AllApps.xaml.cs
+++ Views/AllApps.xaml.cs
@@ -25,8 +25,10 @@ namespace MvvmTutorials.ToolkitMessages.Views;
 public partial class AllApps : UserControl
 {
     public AppTimeModel appTimeModel = new AppTimeModel();
+    public textcombine allAppsTextGlobal = new textcombine();
     public void setDayTimerForApps(DateTime dt)
     {
+/*        MessageBox.Show("day");*/
         List<AppTimer> appTimerList = new List<AppTimer>();
         foreach(var i in DataList)
         {
@@ -44,9 +46,53 @@ public partial class AllApps : UserControl
         AllAppsTimer.DataContext = appTimeModel;
 
     }
+    public void setWeekTimerForApps(DateTime dt)
+    {
+/*        MessageBox.Show("week");*/
+        List<AppTimer> appTimerList = new List<AppTimer>();
+        foreach (var i in DataList)
+        {
+            if (!i.IsSelected)
+                continue;
+            var timerList = GlobalData.DataInstance.GetProcessWeekLogList(i.Index, dt);
+            int lengthAll = 0;
+            foreach(var timer in timerList)
+            {
+                lengthAll += timer.Time;
+            }
+            appTimerList.Add(new AppTimer(i.Name, lengthAll));
+        }
+        AppTimeModel newAppTimeModel = new AppTimeModel();
+        newAppTimeModel.appTimers = appTimerList;
+        appTimeModel = newAppTimeModel;
+        AllAppsTimer.DataContext = appTimeModel;
+
+    }
+    public void setMonthTimerForApps(DateTime dt)
+    {
+/*        MessageBox.Show("month");*/
+        List<AppTimer> appTimerList = new List<AppTimer>();
+        foreach (var i in DataList)
+        {
+            if (!i.IsSelected)
+                continue;
+            var timerList = GlobalData.DataInstance.GetProcessMonthLogList(i.Index, dt);
+            int lengthAll = 0;
+            foreach (var timer in timerList)
+            {
+                lengthAll += timer.Time;
+            }
+            appTimerList.Add(new AppTimer(i.Name, lengthAll));
+        }
+        AppTimeModel newAppTimeModel = new AppTimeModel();
+        newAppTimeModel.appTimers = appTimerList;
+        appTimeModel = newAppTimeModel;
+        AllAppsTimer.DataContext = appTimeModel;
+
+    }
     public void OnChecked(object sender, RoutedEventArgs e)
     {
-        setDayTimerForApps(DateTime.Now);
+        setDayTimerForApps(new DateTime(2023,5,31));
         MessageBox.Show(DataList[1].Name + DataList[1].IsSelected.ToString());
     }
     private void CheckBox_Click(object sender, RoutedEventArgs e)
@@ -58,14 +104,12 @@ public partial class AllApps : UserControl
             app selectItem = (app)(cntr as DataGridRow).DataContext; //这里是把选中行转换为对象,进而拿到CheckBox中绑定的名字
             selectItem.IsSelected = !selectItem.IsSelected;//这里是拿到MyList类中的isEnable属性 即上面 xmal中 Checkbox中绑定的变量
 
-            string str = "";
-            foreach (var vItem in DataList)
-            {
-                
-                str += vItem.Name;
-                str += vItem.IsSelected.ToString() + "\n";
-            }
-            setDayTimerForApps(DateTime.Now);
+            if(allAppsTextGlobal.day)
+                setDayTimerForApps(new DateTime(2023,5,31));
+            else if(allAppsTextGlobal.week)
+                setWeekTimerForApps(new DateTime(2023,5,31));
+            else
+                setDayTimerForApps(new DateTime(2023,5,31));
         }
     }
     public AllApps()
@@ -73,11 +117,16 @@ public partial class AllApps : UserControl
         InitializeComponent();
 
         //右下角文字数据更改
-        textcombine Allappstext = new textcombine();
-        Allappstext.text1 = "本周共使用20个应用程序";
-        Allappstext.text2 = "一共100分钟";
-        Allappstext.text3 = "平均一天使用14分钟";
-        Allapptexts.DataContext = Allappstext;
+        textcombine initText = new textcombine();
+        initText.text1 = "本周共使用20个应用程序";
+        initText.text2 = "一共100分钟";
+        initText.text3 = "平均一天使用14分钟";
+        initText.day = true;
+        initText.week = false;
+        initText.month = false;
+        allAppsTextGlobal = initText;
+        Allapptexts.DataContext = allAppsTextGlobal;
+        ShowMode.DataContext = allAppsTextGlobal;
         var appList=GlobalData.AppDataInstance.GetAllApps();
 
         //左侧应用修改
@@ -94,7 +143,7 @@ public partial class AllApps : UserControl
         }
         Appname.DataContext = this;
         AllAppsTimer.DataContext = appTimeModel;
-        setDayTimerForApps(DateTime.Now);
+        setDayTimerForApps(new DateTime(2023,5,31));
     }
 
 
@@ -152,14 +201,22 @@ public partial class AllApps : UserControl
         //添加默认起始日期
         box.Text = "2023.3.1";
         //其他数据更改
+        allAppsTextGlobal.day = false;
+        allAppsTextGlobal.week = false;
+        allAppsTextGlobal.month = true;
 
+        setMonthTimerForApps(new DateTime(2023,5,31));
     }
     private void Week(object sender, RoutedEventArgs e)
     {
         //添加默认起始日期
         box.Text = "2023.2.1";
         //其他数据更改
+        allAppsTextGlobal.day = false;
+        allAppsTextGlobal.week = true;
+        allAppsTextGlobal.month = false;
 
+        setWeekTimerForApps(new DateTime(2023,5,31));
     }
 
     private void Day(object sender, RoutedEventArgs e)
@@ -167,7 +224,11 @@ public partial class AllApps : UserControl
         //添加默认起始日期
         box.Text = "2023.1.1";
         //其他数据更改
+        allAppsTextGlobal.day = true;
+        allAppsTextGlobal.week = false;
+        allAppsTextGlobal.month = false;
 
+        setDayTimerForApps(new DateTime(2023,5,31));
     }
 
     //确认按钮
