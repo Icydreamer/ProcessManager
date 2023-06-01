95ba858a0179804374010854f03970bf66ecf717
Icydreamer <1091939105@qq.com>
2023-06-01 16:41:08 +0800

    修复查询周数据函数的bug

--- DataBase/Services/Data.cs
+++ DataBase/Services/Data.cs
@@ -321,38 +321,36 @@ namespace DataBase.Services
                 return new List<DailyLogModel>();
             }
         }
-
-        /// <summary> 
-        /// 判断两个日期是否在同一周 
-        /// </summary> 
-        /// <param name="dtmS">开始日期</param> 
-        /// <param name="dtmE">结束日期</param>
-        /// <returns></returns> 
-        private bool IsInSameWeek(DateTime dtmS, DateTime dtmE)
+        /// <summary>
+        /// 获取指定进程某周的数据
+        /// </summary>
+        /// <param name="processName"></param>
+        /// <param name="week"></param>
+        /// <returns></returns>
+        public List<DailyLogModel> GetProcessWeekLogList(int AppID, DateTime week)
         {
-            TimeSpan ts = dtmE - dtmS;
-            double dbl = ts.TotalDays;
-            int intDow = Convert.ToInt32(dtmE.DayOfWeek);
-            if (intDow == 0) intDow = 7;
-            if (dbl >= 7 || dbl >= intDow) return false;
-            else return true;
-        }
-            /// <summary>
-            /// 获取指定进程某周的数据
-            /// </summary>
-            /// <param name="processName"></param>
-            /// <param name="week"></param>
-            /// <returns></returns>
-            public List<DailyLogModel> GetProcessWeekLogList(int AppID, DateTime week)
-        {
+            DateTime weekStartDate = week, weekEndDate = week;
+            if (week.DayOfWeek == DayOfWeek.Monday)
+            {
+                weekStartDate = week.Date;
+                weekEndDate = week.Date.AddDays(6);
+            }
+            else
+            {
+                int weekNum = (int)week.DayOfWeek;
+                if (weekNum == 0)
+                {
+                    weekNum = 7;
+                }
+                weekNum -= 1;
+                weekStartDate = week.Date.AddDays(-weekNum);
+                weekEndDate = weekStartDate.Date.AddDays(6);
+            }
             using (var db = dataBase.GetReaderContext())
             {
-
                 var res = db.DailyLog.Include(m => m.AppModel).Where(
                 m =>
-                m.Date.Year == week.Year
-                && m.Date.Month == week.Month
-                && m.Date.AddDays(-(int)m.Date.DayOfWeek).Date== week.AddDays(-(int)week.DayOfWeek).Date
+                m.Date >= weekStartDate && m.Date <= weekEndDate
                 && m.AppModelID == AppID
                 );
                 if (res != null)
